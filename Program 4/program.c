4a525ad8a7945792afa7102f3ef5285faab19739
iamvinitk <kananivinit97@gmail.com>
2018-05-16 11:11:38 +0530

    Final

--- Program 4/program.c
+++ Program 4/program.c
@@ -8,7 +8,7 @@ char act[15];
 void check();
 
 void main()
- {
+{
   printf("\n\t\t SHIFT REDUCE PARSER\n");
   printf("\n GRAMMER\n");
   printf("\n E->E+T|T\n T->T* F | F");
@@ -22,58 +22,58 @@ void main()
   strcpy(act,"Shift ");
   if (ip_sym[ip_ptr]=='(')
   {
-   temp[0]=ip_sym[ip_ptr];
-   temp[1]='\0';
+    temp[0]=ip_sym[ip_ptr];
+    temp[1]='\0';
   }
   else
   {
-   temp[0]=ip_sym[ip_ptr];
-   temp[1]=ip_sym[ip_ptr+1];
-   temp[2]='\0';
+    temp[0]=ip_sym[ip_ptr];
+    temp[1]=ip_sym[ip_ptr+1];
+    temp[2]='\0';
   }
 
   strcat(act,temp);
   len=strlen(ip_sym);
-  
- for(i=0;i<=len-1;i++)
+
+  for(i=0;i<=len-1;i++)
   {
-   if(ip_sym[ip_ptr]=='i' &&ip_sym[ip_ptr+1]=='d')
-   {
-    stack[st_ptr]=ip_sym[ip_ptr];
+    if(ip_sym[ip_ptr]=='i' &&ip_sym[ip_ptr+1]=='d')
+    {
+      stack[st_ptr]=ip_sym[ip_ptr];
+      st_ptr++;
+      ip_sym[ip_ptr]=' ';
+      ip_ptr++;
+      stack[st_ptr]= ip_sym[ip_ptr];
+      stack[st_ptr+1]='\0';
+      ip_sym[ip_ptr]=' ';
+      ip_ptr++;
+    }
+    else
+    {
+      stack[st_ptr]=ip_sym[ip_ptr];
+      stack[st_ptr+1]='\0';
+      ip_sym[ip_ptr]=' ';
+      ip_ptr++;
+    }
+
+    printf("\n $%s\t\t\t%s$\t\t\t%s",stack,ip_sym,act); /* second print with action shift*/
+    strcpy(act,"shift ");
+    if (ip_sym[ip_ptr]=='(' || ip_sym[ip_ptr]=='*' || ip_sym[ip_ptr]=='+' || ip_sym[ip_ptr]==')')
+    {
+      temp[0]=ip_sym[ip_ptr];
+      temp[1]='\0';
+    }
+    else
+    {
+      temp[0]=ip_sym[ip_ptr];
+      temp[1]=ip_sym[ip_ptr+1];
+      temp[2]='\0';
+    }
+    strcat(act,temp);
+    check();
     st_ptr++;
-    ip_sym[ip_ptr]=' ';
-    ip_ptr++;
-    stack[st_ptr]= ip_sym[ip_ptr];
-    stack[st_ptr+1]='\0';
-    ip_sym[ip_ptr]=' ';
-    ip_ptr++;
-   }
-  else
-  {
-   stack[st_ptr]=ip_sym[ip_ptr];
-   stack[st_ptr+1]='\0';
-   ip_sym[ip_ptr]=' ';
-   ip_ptr++;
   }
-  
-  printf("\n $%s\t\t\t%s$\t\t\t%s",stack,ip_sym,act); /* second print with action shift*/
-  strcpy(act,"shift ");
-  if (ip_sym[ip_ptr]=='(' || ip_sym[ip_ptr]=='*' || ip_sym[ip_ptr]=='+' || ip_sym[ip_ptr]==')')
-  {
-   temp[0]=ip_sym[ip_ptr];
-   temp[1]='\0';
-  }
-  else
-  {
-   temp[0]=ip_sym[ip_ptr];
-   temp[1]=ip_sym[ip_ptr+1];
-   temp[2]='\0';
-  }
-  strcat(act,temp);
-  check();
-  st_ptr++;
- }
- 
+
   st_ptr++;
   check();
 }
@@ -84,86 +84,85 @@ void check()
   int flag=0;
   while(1)
   {
-   if (stack[st_ptr]=='d'&& stack[st_ptr-1]=='i')
-   {
-    stack[st_ptr-1]='F';
-    stack[st_ptr]='\0';
-    st_ptr--;
-    flag=1;
-    printf("\n $%s\t\t\t%s$\t\t\tF->id",stack, ip_sym);
-   }
-  
-   if (stack[st_ptr]==')' && stack[st_ptr-1]=='E' && stack[st_ptr-2]=='(')
-   {
-    stack[st_ptr-2]='F';
-    stack[st_ptr-1]='\0';
-    flag=1;
-    st_ptr=st_ptr-2;
-    printf("\n $%s\t\t\t%s$\t\t\tF->id",stack, ip_sym);
-   }
-   
-   if (stack[st_ptr]=='F' && stack[st_ptr-1]=='*' && stack[st_ptr-2]=='T')
-   {
-     // stack[st_ptr-2]='T';
-     stack[st_ptr-1]='\0';
-     st_ptr= st_ptr-2;
-     flag=1;
-     printf("\n $%s\t\t\t%s$\t\t\tT->T*F",stack, ip_sym);
-   }
- 
-   else
-   {
-     if (stack[st_ptr]=='F')
-     {
-       stack[st_ptr]='T';
-       flag=1;
-       printf("\n $%s\t\t\t%s$\t\t\tT->F",stack, ip_sym);
-     }
-   }
-     
-     if( stack[st_ptr]=='T' && stack[st_ptr-1]=='+' && stack[st_ptr-2]=='E' && ip_sym[ip_ptr]!='*' )
-     {
+    if (stack[st_ptr]=='d'&& stack[st_ptr-1]=='i')
+    {
+      stack[st_ptr-1]='F';
+      stack[st_ptr]='\0';
+      st_ptr--;
+      flag=1;
+      printf("\n $%s\t\t\t%s$\t\t\tF->id",stack, ip_sym);
+    }
+
+    if (stack[st_ptr]==')' && stack[st_ptr-1]=='E' && stack[st_ptr-2]=='(')
+    {
+      stack[st_ptr-2]='F';
+      stack[st_ptr-1]='\0';
+      flag=1;
+      st_ptr=st_ptr-2;
+      printf("\n $%s\t\t\t%s$\t\t\tF->id",stack, ip_sym);
+    }
+
+    if (stack[st_ptr]=='F' && stack[st_ptr-1]=='*' && stack[st_ptr-2]=='T')
+    {
+      // stack[st_ptr-2]='T';
+      stack[st_ptr-1]='\0';
+      st_ptr= st_ptr-2;
+      flag=1;
+      printf("\n $%s\t\t\t%s$\t\t\tT->T*F",stack, ip_sym);
+    }
+
+    else
+    {
+      if (stack[st_ptr]=='F')
+      {
+        stack[st_ptr]='T';
+        flag=1;
+        printf("\n $%s\t\t\t%s$\t\t\tT->F",stack, ip_sym);
+      }
+    }
+
+    if( stack[st_ptr]=='T' && stack[st_ptr-1]=='+' && stack[st_ptr-2]=='E' && ip_sym[ip_ptr]!='*' )
+    {
       //stack[st_ptr-2]='E';
       stack[st_ptr-1]='\0';
       st_ptr= st_ptr-2;
       flag=1;
       printf("\n $%s\t\t\t%s$\t\t\tE->E+T",stack, ip_sym);
-     }
-     
-     else if ((stack[st_ptr]=='T' && ip_sym[ip_ptr]== '+') || (stack[0]=='T' && ip_sym[ip_ptr]== '\0') ||
-             (stack[st_ptr]=='T' && ip_sym[ip_ptr]== ')'))
-     {
-       stack[st_ptr]='E';
-       flag=1;
-       printf("\n $%s\t\t\t%s$\t\t\tE->T",stack, ip_sym);
-     } 
-    
-     if((stack[st_ptr]=='T' && ip_sym[ip_ptr]== '*') ||(stack[st_ptr]=='E' && ip_sym[ip_ptr]==')')||
-        (stack[st_ptr]=='E' && ip_sym[ip_ptr]=='+')||(stack[st_ptr]=='+'&& ip_sym[ip_ptr]=='i' && ip_sym[ip_ptr+1]=='d')||
-        (stack[st_ptr]== '(' && ip_sym[ip_ptr]=='i' && ip_sym[ip_ptr+1]=='d') ||(stack[st_ptr]== '(' && ip_sym[ip_ptr]=='(')||
-        (stack[st_ptr]=='*'&& ip_sym[ip_ptr]=='i' && ip_sym[ip_ptr+1]=='d' ) ||
-        (stack[st_ptr]=='*'&& ip_sym[ip_ptr]=='(') ||
-        (stack[st_ptr]=='+'&& ip_sym[ip_ptr]=='('))
-	     {
-		flag=2;
-	     }
-    
-     if(!strcmp(stack,"E")&& ip_sym[ip_ptr]=='\0')
-     {
-       printf("\n $%s\t\t\t%s$\t\t\tACCEPT\n",stack,ip_sym);
+    }
 
-       exit(0);
-     }
- 
+    else if ((stack[st_ptr]=='T' && ip_sym[ip_ptr]== '+') || (stack[0]=='T' && ip_sym[ip_ptr]== '\0') ||
+    (stack[st_ptr]=='T' && ip_sym[ip_ptr]== ')'))
+    {
+      stack[st_ptr]='E';
+      flag=1;
+      printf("\n $%s\t\t\t%s$\t\t\tE->T",stack, ip_sym);
+    }
+
+    if((stack[st_ptr]=='T' && ip_sym[ip_ptr]== '*') ||(stack[st_ptr]=='E' && ip_sym[ip_ptr]==')')||
+    (stack[st_ptr]=='E' && ip_sym[ip_ptr]=='+')||(stack[st_ptr]=='+'&& ip_sym[ip_ptr]=='i' && ip_sym[ip_ptr+1]=='d')||
+    (stack[st_ptr]== '(' && ip_sym[ip_ptr]=='i' && ip_sym[ip_ptr+1]=='d') ||(stack[st_ptr]== '(' && ip_sym[ip_ptr]=='(')||
+    (stack[st_ptr]=='*'&& ip_sym[ip_ptr]=='i' && ip_sym[ip_ptr+1]=='d' ) ||
+    (stack[st_ptr]=='*'&& ip_sym[ip_ptr]=='(') ||
+    (stack[st_ptr]=='+'&& ip_sym[ip_ptr]=='('))
+    {
+      flag=2;
+    }
+
+    if(!strcmp(stack,"E")&& ip_sym[ip_ptr]=='\0')
+    {
+      printf("\n $%s\t\t\t%s$\t\t\tACCEPT\n",stack,ip_sym);
+
+      exit(0);
+    }
+
     if(flag==0)
     {
       printf("\n%s\t\t\t%s\t\t\treject\n",stack,ip_sym);
       exit(0);
     }
-   
-   if (flag==2) 
-    return;
-   flag=0;
- }
-}
 
+    if (flag==2)
+    return;
+    flag=0;
+  }
+}
--- Program 5/program.c
+++ Program 5/program.c
@@ -10,59 +10,42 @@ void main()
 	FILE *fp1,*fp2;
 	fp1=fopen("input.txt","r");
 	fp2=fopen("output.txt","w");
-	while(!feof(fp1))
+	char buffer[30];
+	//while(!feof(fp1))
+	while (!feof(fp1))
 	{
 		fscanf(fp1,"%s%s%s%s",result,arg1,op,arg2);
-
+		printf("%s %s %s %s\n",result, arg1, op, arg2);
 		if(strcmp(op,"+")==0)
 		{
 			fprintf(fp2,"\nMOV R0,%s",arg1);
 			fprintf(fp2,"\nADD R0,%s",arg2);
-
 			fprintf(fp2,"\nMOV %s,R0",result);
-
-		
 		}
 		if(strcmp(op,"-")==0)
 		{
 			fprintf(fp2,"\nMOV R0,%s",arg1);
 			fprintf(fp2,"\nSUB R0,%s",arg2);
-
 			fprintf(fp2,"\nMOV %s,R0",result);
-
-		
 		}
 		if(strcmp(op,"*")==0)
 		{
 			fprintf(fp2,"\nMOV R0,%s",arg1);
 			fprintf(fp2,"\nMUL R0,%s",arg2);
-
 			fprintf(fp2,"\nMOV %s,R0",result);
-
-		
 		}
 		if(strcmp(op,"/")==0)
 		{
 			fprintf(fp2,"\nMOV R0,%s",arg1);
 			fprintf(fp2,"\nDIV R0,%s",arg2);
-
 			fprintf(fp2,"\nMOV %s,R0",result);
-
-		
 		}
 		if(strcmp(op,"=")==0)
 		{
 			fprintf(fp2,"\nMOV R0,%s",arg1);
-			
-
 			fprintf(fp2,"\nMOV %s,R0",result);
-
-		
 		}
 	}
-
-fclose(fp1);
-fclose(fp2);
-
-
+	fclose(fp1);
+	fclose(fp2);
 }
